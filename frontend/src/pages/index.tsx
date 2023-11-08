1481fec9d583389d8e6696dde3fd5204b41046fa
Koichi Omae <ohmae@arubeh.com>
2023-11-08 18:56:18 +0900

    不要なデータの削除

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,3 @@
-import { Button } from '@/components/Index/sample/Button';
 export default function Home() {
-  return (
-    <div className='py-2 font-bold text-red-500'>
-      Template
-      <Button />
-    </div>
-  );
+  return <div className='py-2 font-bold text-red-500'>Template</div>;
 }
